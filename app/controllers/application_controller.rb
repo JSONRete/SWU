20f3ed256588cf9cfe52bfd9ceb7e3a45a9a3726
JSONRete <jsonrete@gmail.com>
2022-04-02 16:52:42 -1100

    printed out ''Hello, World!''

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@
 class ApplicationController < Sinatra::Base
     configure do
     set :public_folder, 'public'
-    set :views, 'app/views' enable :sessions
+    # set :views, 'app/views' enable :sessions
     set :session_secret, 'password_security'
-    # end
+    end
     
     get '/' do
     'Hello, World!'
