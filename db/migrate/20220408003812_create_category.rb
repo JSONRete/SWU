178169f688fa1021d6aac3af873ab071ac0df5d3
JSONRete <jsonrete@gmail.com>
2022-04-09 16:58:05 -1100

    gettin ready to set category migrations

--- db/migrate/20220408003812_create_category.rb
+++ db/migrate/20220408003812_create_category.rb
@@ -3,6 +3,8 @@ class CreateCategory < ActiveRecord::Migration[5.2]
     create_table :categories do |t|
       t.string 
 
+      # Planets, Spaceships, Vehicles, People, Films and Species
+
       t.timestamps
     end
   end
